23df1457aa590ff8d3083b75c412280373ee4b14
Volodya-46 <volodya@noblescripts.com>
2024-01-24 13:59:34 +0400

    update

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:3001',
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+
 // middlewares
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -31,6 +41,60 @@ morgan(function (tokens, req, res) {
     ].join(' ')
 })
 
+// sockets
+
+interface IConnectedUser {
+    id: string
+    name: string
+    isReady: boolean
+}
+
+let connectedUsers: IConnectedUser[] = [];
+let gameIsStarted = false
+
+io.on('connection', (socket: any) => {
+    socket.on('join', (user: IConnectedUser) => {
+        connectedUsers.push({...user, id: socket.id});
+        io.emit('updateUsers', connectedUsers);
+    });
+
+    socket.on('ready', (isReady: boolean) => {
+        connectedUsers.forEach((user) => {
+            if (user.id === socket.id) {
+                user.isReady = isReady
+            }
+        })
+
+        const allUsersReady = connectedUsers.every(user => user.isReady);
+        io.emit('usersReady', allUsersReady);
+        io.emit('updateUsers', connectedUsers)
+    });
+
+    socket.on('startGame', (bool: boolean) => {
+        gameIsStarted = bool
+        socket.emit('startGame', gameIsStarted)
+    })
+
+    socket.on('winner', (name: string) => {
+        io.emit('gameOver', name);
+        gameIsStarted = false
+    });
+
+    socket.on('disconnect', () => {
+        const userIndex = connectedUsers.findIndex(user => user.id === socket.id);
+        if (userIndex !== -1) {
+            connectedUsers.splice(userIndex, 1);
+            io.emit('updateUsers', connectedUsers);
+        }
+
+
+        if (connectedUsers.length === 1 && gameIsStarted) {
+            io.emit('gameOver', connectedUsers[0].name)
+            gameIsStarted = false
+        }
+    });
+});
+
 // routes
 
 import indexRoute from './routes'
@@ -48,7 +112,7 @@ import { connectToDatabase } from './database';
 
 connectToDatabase()
   .then(() => {
-      app.listen(PORT, () => {
+      http.listen(PORT, () => {
           console.log(`Server is running on port ${PORT}`);
       });
   })
